af914f525bf10ea581d19d7281db70a33ff1c0bd
Shubh Bagaria <shubhbagaria1@gmail.com>
2026-08-12 20:04:00 +0530

    Model Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass  #THIS IS A WRAPPER WHICH MAKES UP FOR THE MISSING __INIT__ IN THE CLASS BELOW
 class DataIngestionConfig:
@@ -49,4 +51,7 @@ if __name__=='__main__':
     train_data_path,test_data_path=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
